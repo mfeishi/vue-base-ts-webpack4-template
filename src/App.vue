59d6a73e9f6ea1c9b6c45d666ee5542a73676d49
wangran <ran.wang@aihuishou.com>
2020-07-27 16:18:48 +0800

    base

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="app">
+		1111
+		<div class="box">
+			{{name}}
+		</div>
+		
+	</div>
+</template>
+
+<script lang='ts'>
+	import {
+		Component,
+		Prop,
+		Vue,
+		Watch
+	} from 'vue-property-decorator';
+
+	export default class App extends Vue {
+		private name:string  = 'template';
+		private isShowNav:boolean = true;
+		mounted(){
+			console.error('mmm')
+		}
+	
+	}
+</script>
+
+<style lang="less">
+	.app{
+		.box{
+			background: lavender;
+		}
+		
+	}
+</style>
