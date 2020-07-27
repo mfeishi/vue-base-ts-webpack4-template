6f4f30a797947f8ba337ca8e6fdd7d75510f295a
wangran <ran.wang@aihuishou.com>
2020-07-27 11:20:01 +0800

    base

---
+++ build/webpack.base.conf.ts
@@ -0,0 +1,10 @@
+// const webpack = require('webpack');
+// const {
+// 	resolve
+// } = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const {
+// 	VueLoaderPlugin
+// } = require("vue-loader");
+// const srcPath = resolve(__dirname, '..', 'src');
